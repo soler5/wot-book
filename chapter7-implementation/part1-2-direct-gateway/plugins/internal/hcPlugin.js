47ae6c851f62354c207580d94075017341b13aae
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-11 18:20:46 +0100

    mqtt

--- chapter7-implementation/part1-2-direct-gateway/plugins/internal/hcPlugin.js
+++ chapter7-implementation/part1-2-direct-gateway/plugins/internal/hcPlugin.js
@@ -1,10 +1,17 @@
+var mqtt = require('mqtt');
 var resources = require('./../../resources/model'),
   utils = require('./../../utils/utils.js');
+
+  var client = mqtt.connect("mqtts://192.168.0.37:8883");
+  
+
 
 var interval, sensor;
 var model = resources.pi.sensors;
 var pluginName = 'US sensor';
 var localParams = {'simulate': false, 'frequency': 5000};
+
+
 
 exports.start = function (params) {
   localParams = params;
@@ -26,32 +33,39 @@ exports.stop = function () {
 
 function connectHardware() {
 
-  const Gpio = require('pigpio').Gpio;
+  
+
+const Gpio = require('pigpio').Gpio;
 
 const MICROSECONDS_PER_CM = 1e6/34321;
 
 const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
 const ECHO = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
-trigger.digitalWrite(0);
+client.on('connect', function () {
+  trigger.digitalWrite(0);
 
-const warchHCSR04 = () =>{
-  var starTick;
-  ECHO.on('alert', (level, tick)=>{
-    if (level==1){
-      starTick = tick;
-    }else{
-      const endTick = tick;
-      const diff = (endTick>>0) - (starTick>>0);
-      model.hc.value = diff/2/MICROSECONDS_PER_CM;
-    }
-  })
-}
-warchHCSR04();
+  const warchHCSR04 = () =>{
+    var starTick;
+    ECHO.on('alert', (level, tick)=>{
+      if (level==1){
+        starTick = tick;
+      }else{
+        const endTick = tick;
+        const diff = (endTick>>0) - (starTick>>0);
+        model.hc.value = diff/2/MICROSECONDS_PER_CM;
+        client.publish('HC-SR04', '[{"value": '+value+'}]');
+      }
+    })
+  }
+  warchHCSR04();
 
-setInterval(()=>{
-  trigger.trigger(10, 1);
-}, 1000);
+  setInterval(()=>{
+    trigger.trigger(10, 1);
+  }, 1000);
+});
+
+
 };
 
 function simulate() {
