73dea0bc45b73e55c53d3979fa3b24be43765760
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-11 18:36:59 +0100

    no message

--- chapter7-implementation/part1-2-direct-gateway/plugins/internal/hcPlugin.js
+++ chapter7-implementation/part1-2-direct-gateway/plugins/internal/hcPlugin.js
@@ -2,7 +2,7 @@ var mqtt = require('mqtt');
 var resources = require('./../../resources/model'),
   utils = require('./../../utils/utils.js');
 
-  var client = mqtt.connect("mqtts://raspberry");
+  var client = mqtt.connect("localhost");
   
 
 
