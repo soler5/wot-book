6ff622051db3b005a7ca6dd2dc15e6971d9d9405
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-13 18:08:39 +0100

    Fix

--- chapter7-implementation/part1-2-direct-gateway/wot-server.js
+++ chapter7-implementation/part1-2-direct-gateway/wot-server.js
@@ -11,9 +11,9 @@ var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-pirPlugin.start({'simulate': true, 'frequency': 2000}); //#B
+pirPlugin.start({'simulate': false, 'frequency': 2000}); //#B
 ledsPlugin.start({'simulate': false, 'frequency': 10000}); //#B
-dhtPlugin.start({'simulate': true, 'frequency': 10000}); //#B
+dhtPlugin.start({'simulate': false, 'frequency': 10000}); //#B
 hcPlugin.start({'simulate': false, 'frequency': 10000}); //#B
 // External Plugins
 //var coapPlugin = require('./plugins/external/coapPlugin');
