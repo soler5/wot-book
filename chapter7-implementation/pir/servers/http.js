45deabf9835be8faa72a86c329aead50639384e6
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-14 17:43:04 +0100

    no message

--- chapter7-implementation/pir/servers/http.js
+++ chapter7-implementation/pir/servers/http.js
@@ -1,9 +1,7 @@
 // Final version
 var express = require('express'),
-  actuatorsRoutes = require('./../routes/actuators'),
   sensorRoutes = require('./../routes/sensors'),
   thingsRoutes = require('./../routes/things'),
-  resources = require('./../resources/model'),
   converter = require('./../middleware/converter'),
   cors = require('cors'),
   bodyParser = require('body-parser');
@@ -14,7 +12,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/pi/actuators', actuatorsRoutes);
 app.use('/pi/sensors', sensorRoutes);
 app.use('/things', thingsRoutes);
 
@@ -25,34 +22,3 @@ app.get('/pi', function (req, res) {
 // For representation design
 app.use(converter());
 module.exports = app;
-
-
-/*
- //Initial version:
-
-var express = require('express'),
-  actuatorsRoutes = require('./../routes/actuators'),
-  sensorRoutes = require('./../routes/sensors'),
-  resources = require('./../resources/model'), //#A
-  cors = require('cors'); 
-
-var app = express(); //#B
-
-app.use(cors()); //#C
-
-app.use('/pi/actuators', actuatorsRoutes); //#D
-app.use('/pi/sensors', sensorRoutes);
-
-app.get('/pi', function (req, res) { //#E
-  res.send('This is the WoT-Pi!')
-});
-
-module.exports = app;
-
-//#A Requires the Express framework, your routes, and the model
-//#B Creates an application with the Express framework; this wraps an HTTP server
-//#C Enable CORS support (see section 6.1.5)
-//#D Binds your routes to the Express application; bind them to /pi/actuators/... and /pi/sensors/...
-//#E Create a default route for /pi
-
-*/
