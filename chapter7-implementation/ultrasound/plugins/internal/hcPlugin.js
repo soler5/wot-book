b747821d86580b80c57d53b24b72d069a5c03dde
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-19 17:07:16 +0100

    no message

---
+++ chapter7-implementation/ultrasound/plugins/internal/hcPlugin.js
@@ -0,0 +1,69 @@
+//var mqtt = require('mqtt');
+var resources = require('./../../resources/model'),
+utils = require('./../../utils/utils.js');
+
+//var client = mqtt.connect("mqtt://192.168.0.37:1883");
+  
+
+
+var interval, sensor;
+var model = resources.pi.sensors;
+var pluginName = 'US sensor';
+var localParams = {'simulate': false, 'frequency': 5000};
+
+
+
+exports.start = function (params) {
+  localParams = params;
+  connectHardware();
+};
+
+exports.stop = function () {
+  if (params.simulate) {
+    clearInterval(interval);
+  } else {
+    sensor.unexport();
+  }
+  console.info('%s plugin stopped!', pluginName);
+};
+
+function connectHardware() {
+
+  const Gpio = require('pigpio').Gpio;
+  const MICROSECONDS_PER_CM = 1e6/34321;
+
+  const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+  const ECHO = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+
+//client.on('connect', function () {
+  trigger.digitalWrite(0);
+
+  const warchHCSR04 = () =>{
+    var starTick;
+    ECHO.on('alert', (level, tick)=>{
+      if (level==1){
+        starTick = tick;
+      }else{
+        const endTick = tick;
+        const diff = (endTick>>0) - (starTick>>0);
+        model.hc.value = diff/2/MICROSECONDS_PER_CM;
+        //client.publish('HC-SR04', '[{"value": '+model.hc.value+'}]');
+      }
+    })
+  }
+  warchHCSR04();
+
+  setInterval(()=>{
+    trigger.trigger(10, 1);
+  }, 1000);
+//});
+};
+
+function showValue() {
+  console.info('Us sensor distance: %s mm \%', model.hc);
+};
+
+//#A Initialize the driver for HC on GPIO 12 (as specified in the model)
+//#B Fetch the values from the sensors
+//#C Update the model with the new temperature and humidity values; note that all observers will be notified
+//#D Because the driver doesn’t provide interrupts, you poll the sensors for new values on a regular basis with a regular timeout function and set sensor.read() as a callback
---
+++ chapter7-implementation/ultrasound/plugins/internal/pirPlugin.js
@@ -0,0 +1,49 @@
+var resources = require('./../../resources/model');
+var mqtt = require('mqtt');
+var interval, sensor;
+var model = resources.pi.sensors.pir;
+var pluginName = resources.pi.sensors.pir.name;
+var localParams = {'simulate': false, 'frequency': 2000};
+
+var client = mqtt.connect("mqtt://192.168.0.37:1883");
+
+exports.start = function (params) { //#A
+  localParams = params;
+
+  client.on('connect', function () {
+    connectHardware();
+  });
+};
+
+exports.stop = function () { //#A
+  if (localParams.simulate) {
+    clearInterval(interval);
+  } else {
+    sensor.unexport();
+  }
+  console.info('%s plugin stopped!', pluginName);
+};
+
+function connectHardware() { //#B
+  var Gpio = require('onoff').Gpio;
+  sensor = new Gpio(model.gpio, 'in', 'both'); //#C
+  sensor.watch(function (err, value) { //#D
+    if (err) exit(err);
+    model.value = !!value;
+    showValue();
+    client.publish('Sensor-Pir', model.value ? 'there is someone!' : 'not anymore!');
+  });
+  console.info('Hardware %s sensor started!', pluginName);
+};
+
+function showValue() {
+  console.info(model.value ? 'there is someone!' : 'not anymore!');
+};
+
+//#A starts and stops the plugin, should be accessible from other Node.js files so we export them
+//#B require and connect the actual hardware driver and configure it
+//#C configure the GPIO pin to which the PIR sensor is connected
+//#D start listening for GPIO events, the callback will be invoked on events
+//#E allows the plugin to be in simulation mode. This is very useful when developing or when you want to test your code on a device with no sensors connected, such as your laptop.
+
+
