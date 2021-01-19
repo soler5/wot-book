4239f858c68f4488bc2248643ac6d4040db4c008
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-01-19 17:02:23 +0100

    no message

--- chapter7-implementation/pir/routes/sensors.js
+++ chapter7-implementation/pir/routes/sensors.js
@@ -2,9 +2,10 @@
 var express = require('express'),
   router = express.Router(),
   resources = require('./../resources/model');
+  var td = require('./../resources/td.json');
 
 router.route('/').get(function (req, res, next) {
-  req.result = resources.pi.sensors; //#A
+  req.result = td; //#A
   next(); //#B
 });
 
