f54913f866f27e1d2c7fe2d9b550f23757a20b46
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-03-15 13:09:33 +0100

    no message

--- chapter7-implementation/pir/plugins/internal/pirPlugin.js
+++ chapter7-implementation/pir/plugins/internal/pirPlugin.js
@@ -34,7 +34,7 @@ function connectHardware() { //#B
     //console.info(value);
     if(model.value){
       var date = new Date();
-      client.publish('presence', '{value:1,date:'+ date.getDay+'/'+date.getMonth+'/'+date.getFullYear+' '+date.getHours+':'+date.getMinutes+':'+date.getSeconds+'}');
+      client.publish('presence', '{value:1,date:'+ date.getDay()+'/'+date.getMonth()+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+'}');
     }
   });
   console.info('Hardware %s sensor started!', pluginName);
